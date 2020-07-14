a1d1e11e1255c1b940cff668d2b3d8db59739528
Robin Deeboonchai <comtalyst@outlook.com>
2020-07-14 15:48:02 +0700

    Fix for colab

--- dataset_engineering/config.py
+++ dataset_engineering/config.py
@@ -2,13 +2,13 @@
 
 ###### Imports ######
 
-###### Constants ######
-CURRENT_OS = 'Windows'
+###### Constants ###### 
+CURRENT_OS = 'Windows'                # manually change this field every time before deploying (maybe at gdrive)
 
 if CURRENT_OS.lower()[0] == 'w':
   DIR = '.'
 elif CURRENT_OS.lower()[0] == 'c':
-  DIR = 'drive/My Drive/Live Workspace/generative-facial-cosmetics/'
+  DIR = 'drive/My Drive/Live Workspace/generative-facial-cosmetics/dataset_engineering/'
 else:
   raise NotImplementedError('OS is not supported yet')
 
