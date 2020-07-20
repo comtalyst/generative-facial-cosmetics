f71c7d723215e2493fb925c8388e2b319e6b8192
Robin Deeboonchai <comtalyst@outlook.com>
2020-07-20 19:47:17 +0700

    Add data augmentation

--- model_engineering/data/pipeline.py
+++ model_engineering/data/pipeline.py
@@ -6,14 +6,14 @@ from config import *
 import os
 import tensorflow as tf
 from technical.accelerators import BATCH_SIZE as bs
+from PIL import Image
 
 ###### Constants ######
 
 AUTO = tf.data.experimental.AUTOTUNE
-ORIG_IMAGE_SIZE = [360, 360]
 IMAGE_SIZE = [360, 360]
 BATCH_SIZE = bs
-
+JSON = None
 
 ###### Functions ######
 
@@ -21,32 +21,76 @@ BATCH_SIZE = bs
 def read_tfrecord(example):
   features = {
     "image": tf.io.FixedLenFeature([], tf.string),        # tf.string means bytestring
-    "id": tf.io.FixedLenFeature([], tf.int64)             # probably unused, for now
+    "id": tf.io.FixedLenFeature([], tf.int64)
   }
   example = tf.io.parse_example(example, features)
-  images = tf.image.decode_png(example['image'], channels=3)
+  images = tf.image.decode_png(example['image'], channels=4)
   images = tf.cast(images, tf.float32) / 255.0              # convert image to floats in [0, 1] range
   images = tf.image.resize(images, IMAGE_SIZE)              # explicit size will be needed for TPU
-  return images
-  
+  ids = example['id']
+  return (images, ids)
+
+### data augmentation (return the image param if no augmentation is needed)
+def data_augment(data_pair):
+  image, id = data_pair
+
+  ## random saturation
+  image = tf.image.random_saturation(image, 0, 2)
+
+  ## random resize (zoom)
+  coords = JSON[str(id)]
+  min_x = 9999999
+  min_y = 9999999
+  max_x = -9999999
+  max_y = -9999999
+  for coord in coords:
+    x, y = coord
+    min_x = np.min(min_x, x)
+    min_y = np.min(min_y, y)
+    max_x = np.max(max_x, x)
+    max_y = np.max(max_y, y)
+  w = max_x - min_x
+  h = max_y - min_y
+  max_resize = np.max(w/IMAGE_SIZE[0], h/IMAGE_SIZE[1])
+  random_scale = max_resize + np.random.rand()*(1 - max_resize)   # random resize scale without overflowing the image content
+  image = tf.image.central_crop(image, random_scale)              # crop from center
+  image = Image.fromarray(image.numpy()).resize(IMAGE_SIZE)       # resize back using PIL
+  image = tf.convert_to_tensor(np.array(image))                   # convert back to tf tensor
+
+  return (image, id)
+
+### unpack (image, id) to only image
+def unpack(data_pair):
+  return data_pair[0]
+
 ### return "list" of image tensors from specified TFRecords
-def load_dataset(filenames, shuffle=False, batch=False):
+def load_dataset(filenames, shuffle=False, batch=False, augment=False, json=None):
   # read from TFRecords. For optimal performance, read from multiple
   # TFRecord files at once and set the option experimental_deterministic = False
   # to allow order-altering optimizations.
-
   option_no_order = tf.data.Options()
   option_no_order.experimental_deterministic = False
 
   dataset = tf.data.TFRecordDataset(filenames, num_parallel_reads=AUTO)
   dataset = dataset.with_options(option_no_order)
   dataset = dataset.map(read_tfrecord, num_parallel_calls=AUTO)   # dataset from tfrecords is now in a parallel (normal/good) format
+  if augment:
+    JSON = json
+    dataset = dataset.map(data_augment, num_parallel_calls=AUTO)
   if shuffle:
     dataset = dataset.shuffle(len(filenames))
   if batch:
     dataset = dataset.batch(BATCH_SIZE)
     dataset = dataset.prefetch(AUTO) 
+  dataset = dataset.map(unpack, num_parallel_calls=AUTO)
   return dataset
+
+def load_jsons(filenames):
+  json_dict = dict()
+  for fname in filenames:
+    fjson = json.load(open(fname, 'r', encoding='utf-8'))
+    json_dict.update(fjson)
+  return json
 
 ### get filenames
 def get_tfrec_names(dir):
@@ -54,8 +98,17 @@ def get_tfrec_names(dir):
   filenames = tf.io.gfile.glob(PATH_PATTERN)
   return filenames
 
+def get_json_names(dir):
+  PATH_PATTERN = os.path.join(dir, '*.json')
+  filenames = tf.io.gfile.glob(PATH_PATTERN)
+  return filenames
+
 ### just call this ez func
-def get_dataset(bucket_dir, shuffle=False, batch=False):
-  return load_dataset(get_tfrec_names(bucket_dir), shuffle, batch)
+def get_dataset(bucket_dir, shuffle=False, batch=False, augment=False):
+  if augment:
+    json = load_jsons(get_json_names(bucket_dir))       # should fit into memory
+  else:
+    json = None
+  return load_dataset(get_tfrec_names(bucket_dir), shuffle, batch, augment, json)
 
 ###### Execution ######
