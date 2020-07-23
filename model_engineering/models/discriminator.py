bffd59474f922a7dfe2f979b47a287969ecd427f
Robin Deeboonchai <comtalyst@outlook.com>
2020-07-23 13:15:24 +0700

    (WIP) Add progressive learning and rework model saving-loading

--- model_engineering/models/discriminator.py
+++ model_engineering/models/discriminator.py
@@ -17,12 +17,38 @@ class Discriminator:
 
   LATENT_SIZE = 256
   IMAGE_SHAPE = (360, 360, 4)
+  MAX_PROGRESS = 5
   model = None
+
+  current_progress = 0              # for GAN progressive training
 
   ###### Constructor ######
 
   def __init__(self, strategy):
     self.model = self.build_model(strategy)
+
+  ###### Public Methods ######
+
+  def progress(self, strategy):
+    self.model = self.progress_model(self.model, strategy)
+  
+  def save(self, epoch, dir = None):
+    model = self.model
+
+    if dir == None:
+      dir = os.path.join(DIR_OUTPUT, os.path.join('saved_models', 'current'))
+    fname = "discriminator" + "-p_" + str(current_progress) + "-e_" + str(epoch) + ".h5"
+    model.save_weights(fname)
+  
+  # either specify dir/fname or path (path takes priority)
+  def load(self, dir = None, fname = None, path = None):
+    if dir == None:
+      dir = os.path.join(DIR_OUTPUT, os.path.join('saved_models', 'current'))
+    if fname == None and path == None:
+      raise ValueError("Please specify either fname or path")
+    if path == None:
+      path = os.path.join(dir, fname)
+    self.model.load_weights(path)
 
   ###### Functions ######
 
@@ -32,45 +58,81 @@ class Discriminator:
     if reduce_times > 1:
       out = layers.AveragePooling2D(reduce_times)(out)
     return out
+  
+  def output_block(self, input_tensor, units=512):
+    self.OUTPUT_BLOCK_LEN = 4
+
+    x = layers.Conv2D(units, 3, padding = 'same')(input_tensor)
+    x = layers.LeakyReLU(0.2)(x)
+    x = layers.Flatten()(x)
+    # 1-dimensional Neural Network
+    return layers.Dense(1, name="output_prob")(x)
 
   def build_model(self, strategy):
     d_block = self.d_block
+    output_block = self.output_block
 
     with strategy.scope():
       # Image input
       image_input = layers.Input(self.IMAGE_SHAPE, name="input_image")
-
       # Size: 360x360x4
-      x = d_block(image_input, 8)
 
-      # Size: 180x180x8
-      x = d_block(x, 16)
-
-      # Size: 90x90x16
-      x = d_block(x, 32)
-
-      # Size: 45x45x32
-      x = d_block(x, 64, 3)
-
-      # Size: 15x15x64
-      x = d_block(x, 128, 1)
-
-      # Size: 15x15x128
-      x = d_block(x, 256, 3)
-
-      # Size: 5x5x256
-      x = layers.Conv2D(512, 3, padding = 'same')(x)
-      x = layers.LeakyReLU(0.2)(x)
-      x = layers.Flatten()(x)
-
-      # 1-dimensional Neural Network
-      class_output = layers.Dense(1, name="output_prob")(x)
+      # convert to prob for decision
+      class_output = output_block(image_input)
 
       # Make Model
       model = Model(inputs = image_input, outputs = class_output)
 
       model.summary()
 
+    return model
+  
+  ## GAN progressive training: add layers
+  def progress_model(self, model, strategy):
+    d_block = self.d_block
+    output_block = self.output_block
+    current_progress = self.current_progress
+
+    if current_progress >= self.MAX_PROGRESS:
+      print("Maximum progress reached!")
+      return model
+    current_progress += 1
+
+    with strategy.scope():
+      # get last layer before output (before reducing to 1 neuron)
+      x = model.layers[-(self.OUTPUT_BLOCK_LEN+1)].output
+
+      if current_progress == 1:
+        x = d_block(x, 8)
+        # Size: 180x180x8
+
+      elif current_progress == 2:
+        x = d_block(x, 16)
+        # Size: 90x90x16
+
+      elif current_progress == 3:
+        x = d_block(x, 32)
+        # Size: 45x45x32
+
+      elif current_progress == 4:
+        x = d_block(x, 64, 3)
+        # Size: 15x15x64
+        x = d_block(x, 128, 1)
+        # Size: 15x15x128
+
+      elif current_progress == 5:
+        x = d_block(x, 256, 3)
+        # Size: 5x5x256
+      
+      # convert to prob for decision
+      class_output = output_block(x)
+
+      # Make new model on top of the old model
+      model = Model(inputs = model.input, outputs = class_output)
+
+      model.summary()
+    
+    # return progressed model
     return model
 
 
--- model_engineering/models/generator.py
+++ model_engineering/models/generator.py
@@ -17,12 +17,38 @@ class Generator:
 
   LATENT_SIZE = 256
   IMAGE_SHAPE = (360, 360, 4)
+  MAX_PROGRESS = 5
   model = None
+
+  current_progress = 0              # for GAN progressive training
 
   ###### Constructor ######
 
   def __init__(self, strategy):
     self.model = self.build_model(strategy)
+  
+  ###### Public Methods ######
+
+  def progress(self, strategy):
+    self.model = self.progress_model(self.model, strategy)
+  
+  def save(self, epoch, dir = None):
+    model = self.model
+
+    if dir == None:
+      dir = os.path.join(DIR_OUTPUT, os.path.join('saved_models', 'current'))
+    fname = "generator" + "-p_" + str(current_progress) + "-e_" + str(epoch) + ".h5"
+    model.save_weights(fname)
+
+  # either specify dir/fname or path (path takes priority)
+  def load(self, dir = None, fname = None, path = None):
+    if dir == None:
+      dir = os.path.join(DIR_OUTPUT, os.path.join('saved_models', 'current'))
+    if fname == None and path == None:
+      raise ValueError("Please specify either fname or path")
+    if path == None:
+      path = os.path.join(dir, fname)
+    self.model.load_weights(path)
 
   ###### Functions ######
 
@@ -55,10 +81,17 @@ class Generator:
     out = layers.Activation('relu')(out)
     
     return out
+  
+  def output_block(self, input_tensor, channels=4):
+    self.OUTPUT_BLOCK_LEN = 1
+    # make RGBA with values between 0 and 1
+    return layers.Conv2D(channels, 1, padding = 'same', activation = 'sigmoid', name="output_image")(input_tensor)
 
+  ## build model (pre-progress)
   def build_model(self, strategy):
     AdaIN = self.AdaIN
     g_block = self.g_block
+    output_block = self.output_block
 
     with strategy.scope():
       # Latent input
@@ -72,27 +105,10 @@ class Generator:
       # Reshape to 5x5x256
       x = layers.Dense(units=5*5*self.LATENT_SIZE, activation = 'relu')(latent_input)
       x = layers.Reshape([5, 5, self.LATENT_SIZE])(x)
-
       # Size: 5x5x256
-      x = g_block(x, latent, 256, 3)
 
-      # Size: 15x15x256
-      x = g_block(x, latent, 128, 1)
-
-      # Size: 15x15x128
-      x = g_block(x, latent, 64, 3)
-
-      # Size: 45x45x64
-      x = g_block(x, latent, 32)
-
-      # Size: 90x90x32
-      x = g_block(x, latent, 16)
-
-      # Size: 180x180x16
-      x = g_block(x, latent, 8)
-
-      # Size: 360x360x8, make RGB with values between 0 and 1
-      image_output = layers.Conv2D(4, 1, padding = 'same', activation = 'sigmoid', name="output_image")(x)
+      # output RGBA image
+      image_output = output_block(x)
 
       # Make Model
       model = Model(inputs = latent_input, outputs = image_output)
@@ -100,3 +116,53 @@ class Generator:
       model.summary()
 
     return model
+  
+  ## GAN progressive training: add layers
+  def progress_model(self, model, strategy):
+    AdaIN = self.AdaIN
+    g_block = self.g_block
+    output_block = self.output_block
+    current_progress = self.current_progress
+
+    if current_progress >= self.MAX_PROGRESS:
+      print("Maximum progress reached!")
+      return model
+    current_progress += 1
+
+    with strategy.scope():
+      # get last layer before output (before reducing to 4 channels)
+      x = model.layers[-(self.OUTPUT_BLOCK_LEN+1)].output
+
+      if current_progress == 1:
+        x = g_block(x, latent, 256, 3)
+        # Size: 15x15x256
+        x = g_block(x, latent, 128, 1)
+        # Size: 15x15x128
+
+      elif current_progress == 2:
+        x = g_block(x, latent, 64, 3)
+        # Size: 45x45x64
+
+      elif current_progress == 3:
+        x = g_block(x, latent, 32)
+        # Size: 90x90x32
+
+      elif current_progress == 4:
+        x = g_block(x, latent, 16)
+        # Size: 180x180x16
+
+      elif current_progress == 5:
+        x = g_block(x, latent, 8)
+        # Size: 360x360x8
+      
+      # output RGBA image
+      image_output = output_block(x)
+
+      # Make new model on top of the old model
+      model = Model(inputs = model.input, outputs = image_output)
+
+      model.summary()
+    
+    # return progressed model
+    return model
+  
