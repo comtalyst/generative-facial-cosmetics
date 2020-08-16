02fa80b96496541ee766465d04a79248dceb3817
Robin Deeboonchai <comtalyst@outlook.com>
2020-08-16 17:32:50 +0700

    Flatten losses from each pixel for 'gen'
    - also add some clarification comments
    - we should now get the real MSE for 'gen'
    - they are so low because of transparent backgrounds

--- encoder_engineering/trainers/cnn.py
+++ encoder_engineering/trainers/cnn.py
@@ -40,14 +40,24 @@ def load_checkpoint(model, strategy):
 
 @tf.function
 def compute_loss(reals, predictions, loss_type=None, generator=None):
+  # default (mse)
   if loss_type == None:
     return losses.MSE(reals, predictions)
+  # generated image loss
   elif loss_type == 'gen':
+    # generate images from generator
     real_generateds = generator.model(reals, training=False)
     prediction_generateds = generator.model(predictions, training=False)
-    return losses.MSE(real_generateds, prediction_generateds)
+
+    # flatten them to properly find the mean as a value
+    batch_size = reals.shape[0]
+    generated_size_flat = tf.math.reduce_prod(generator.model.output.shape[1:])     # skip 'none' in the first pos
+    new_size = (batch_size, generated_size_flat)
+    return losses.MSE(tf.keras.backend.reshape(real_generateds, new_size), tf.keras.backend.reshape(prediction_generateds, new_size))
+  # default (mse)
   else:
     return losses.MSE(reals, predictions)
+  # return size = batch size (1 loss per batch)
 
 
 @tf.function
@@ -97,14 +107,15 @@ def train(encoder, generator, dataset_gen_func, n_train, n_valid, epochs, strate
 
   allstart = time.time()
   for epoch in range(1, epochs+1):
+    # init
     start = time.time()
     training_dataset, validation_dataset = dataset_gen_func(generator, strategy, batch=True, n_train=n_train, n_valid=n_valid, model_type=model_type)
-    
     training_losses = 0.0
     validation_losses = 0.0
     n_batch_train = 0
     n_batch_valid = 0
 
+    # training steps
     for data_batch in training_dataset:
       training_losses = tf.math.add(training_losses, train_step(model, data_batch, optimizer, strategy, loss_type, generator))
       if FIRSTSTEP:
@@ -112,29 +123,16 @@ def train(encoder, generator, dataset_gen_func, n_train, n_valid, epochs, strate
         FIRSTSTEP = False
       n_batch_train += 1
     
+    # cross validation testing
     for data_batch in validation_dataset:
       validation_losses = tf.math.add(validation_losses, test_step(model, data_batch, strategy, loss_type, generator))
       n_batch_valid += 1
+  
+    # loss summarization
+    training_loss = tf.math.divide(tf.math.reduce_sum(training_losses), n_train)
+    validation_loss = tf.math.divide(tf.math.reduce_sum(validation_losses), n_valid)
 
-    training_units = n_train
-    validation_units = n_valid
-    
-    if loss_type == None:
-      training_units = n_train
-      validation_units = n_valid
-    elif loss_type == 'gen':
-      training_units = tf.math.multiply(n_train, tf.math.divide(tf.math.multiply(encoder.IMAGE_SHAPE[0], tf.math.multiply(encoder.IMAGE_SHAPE[1], encoder.IMAGE_SHAPE[2])), encoder.LATENT_SIZE))
-      validation_units = tf.math.multiply(n_valid, tf.math.divide(tf.math.multiply(encoder.IMAGE_SHAPE[0], tf.math.multiply(encoder.IMAGE_SHAPE[1], encoder.IMAGE_SHAPE[2])), encoder.LATENT_SIZE))
-    else:
-      training_units = n_train
-      validation_units = n_valid
-    
-    # loss is calculated respect to latent size (since it is easy to see differences)
-    
-    
-    training_loss = tf.math.divide(tf.math.reduce_sum(training_losses), tf.dtypes.cast(training_units, tf.float32))
-    validation_loss = tf.math.divide(tf.math.reduce_sum(validation_losses), tf.dtypes.cast(validation_units, tf.float32))
-
+    # reports and checkpoint saving
     print('Epoch {}: Average training loss = {}, Validation loss = {}'.format(epoch, training_loss, validation_loss))
     ckpt.save(checkpoint_path)
     print('Time for epoch {} is {} sec, total {} sec, saved.'.format(epoch, time.time()-start, time.time()-allstart))
