Fix loss calculation

encoder_engineering/trainers/cnn.py
@@ -41,13 +41,13 @@ def load_checkpoint(model, strategy):
 @tf.function
 def compute_loss(reals, predictions, loss_type=None, generator=None):
   if loss_type == None:
-    return losses.MSE(latents, logits)
+    return losses.MSE(reals, predictions)
   elif loss_type == 'gen':
     real_generateds = generator.model(reals, training=False)
     prediction_generateds = generator.model(predictions, training=False)
     return losses.MSE(real_generateds, prediction_generateds)
   else:
-    return losses.MSE(latents, logits)
+    return losses.MSE(reals, predictions)
 
 
 @tf.function
@@ -99,17 +99,42 @@ def train(encoder, generator, dataset_gen_func, n_train, n_valid, epochs, strate
   for epoch in range(1, epochs+1):
     start = time.time()
     training_dataset, validation_dataset = dataset_gen_func(generator, strategy, batch=True, n_train=n_train, n_valid=n_valid, model_type=model_type)
+    
     training_losses = 0.0
     validation_losses = 0.0
+    n_batch_train = 0
+    n_batch_valid = 0
+
     for data_batch in training_dataset:
       training_losses = tf.math.add(training_losses, train_step(model, data_batch, optimizer, strategy, loss_type, generator))
       if FIRSTSTEP:
         print("First batch done!")
         FIRSTSTEP = False
+      n_batch_train += 1
+    
     for data_batch in validation_dataset:
       validation_losses = tf.math.add(validation_losses, test_step(model, data_batch, strategy, loss_type, generator))
-    training_loss = tf.math.divide(tf.math.reduce_sum(training_losses), n_train)
-    validation_loss = tf.math.divide(tf.math.reduce_sum(validation_losses), n_valid)
+      n_batch_valid += 1
+
+    training_units = n_train
+    validation_units = n_valid
+    
+    if loss_type == None:
+      training_units = n_train
+      validation_units = n_valid
+    elif loss_type == 'gen':
+      training_units = tf.math.multiply(n_train, tf.math.divide(tf.math.multiply(encoder.IMAGE_SHAPE[0], tf.math.multiply(encoder.IMAGE_SHAPE[1], encoder.IMAGE_SHAPE[2])), encoder.LATENT_SIZE))
+      validation_units = tf.math.multiply(n_valid, tf.math.divide(tf.math.multiply(encoder.IMAGE_SHAPE[0], tf.math.multiply(encoder.IMAGE_SHAPE[1], encoder.IMAGE_SHAPE[2])), encoder.LATENT_SIZE))
+    else:
+      training_units = n_train
+      validation_units = n_valid
+    
+    # loss is calculated respect to latent size (since it is easy to see differences)
+    
+    
+    training_loss = tf.math.divide(tf.math.reduce_sum(training_losses), tf.dtypes.cast(training_units, tf.float32))
+    validation_loss = tf.math.divide(tf.math.reduce_sum(validation_losses), tf.dtypes.cast(validation_units, tf.float32))
+
     print('Epoch {}: Average training loss = {}, Validation loss = {}'.format(epoch, training_loss, validation_loss))
     ckpt.save(checkpoint_path)
     print('Time for epoch {} is {} sec, total {} sec, saved.'.format(epoch, time.time()-start, time.time()-allstart))